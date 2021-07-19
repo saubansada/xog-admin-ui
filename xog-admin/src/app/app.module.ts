99a3a31179eef7cb4d64bb8ce82154a268306124
ceoxevello <ak.kumshey@gmail.com>
2021-07-19 13:48:25 +0530

    new angular stream

--- xog-admin/src/app/app.module.ts
+++ xog-admin/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SideNavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
