e97736b1e060c12c6192106d4efd8cacdcda04d2
Sauban Sada <sadasauban123@gmail.com>
2021-08-01 14:14:51 +0530

    relocation of components

--- xog-admin/src/app/app-routing.module.ts
+++ xog-admin/src/app/app-routing.module.ts
@@ -1,63 +1,63 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddBrandsComponent } from './components/add-brands/add-brands.component';
-import { AddCategoryComponent } from './components/add-category/add-category.component';
-import { AddOffersComponent } from './components/add-offers/add-offers.component';
-import { AddProductsComponent } from './components/add-products/add-products.component';
-import { AddSubComponent } from './components/add-sub/add-sub.component';
-import { AddUsersComponent } from './components/add-users/add-users.component';
-import { AddWalletComponent } from './components/add-wallet/add-wallet.component';
-import { BrandsComponent } from './components/brands/brands.component';
-import { CategoriesComponent } from './components/categories/categories.component';
-import { CustomersComponent } from './components/customers/customers.component';
-import { FinanceStatsComponent } from './components/finance-stats/finance-stats.component';
-import { ForgetpasswordComponent } from './components/forgetpassword/forgetpassword.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { OffersComponent } from './components/offers/offers.component';
-import { OrdersComponent } from './components/orders/orders.component';
-import { PaymentHistoryComponent } from './components/payment-history/payment-history.component';
-import { PaymentRequestsComponent } from './components/payment-requests/payment-requests.component';
-import { PhoneotpComponent } from './components/phoneotp/phoneotp.component';
-import { ProductsCsvComponent } from './components/products-csv/products-csv.component';
-import { ProductsComponent } from './components/products/products.component';
-import { RefundComponent } from './components/refund/refund.component';
-import { ReturnOrdersComponent } from './components/return-orders/return-orders.component';
+import { AddBrandsComponent } from './views/add-brands/add-brands.component';
+import { AddCategoryComponent } from './views/add-category/add-category.component';
+import { AddOffersComponent } from './views/add-offers/add-offers.component';
+import { AddProductsComponent } from './views/add-products/add-products.component';
+import { AddSubComponent } from './views/add-sub/add-sub.component';
+import { AddUsersComponent } from './views/add-users/add-users.component';
+import { AddWalletComponent } from './views/add-wallet/add-wallet.component';
+import { BrandsComponent } from './views/brands/brands.component';
+import { CategoriesComponent } from './views/categories/categories.component';
+import { CustomersComponent } from './views/customers/customers.component';
+import { FinanceStatsComponent } from './views/finance-stats/finance-stats.component';
+import { ForgetpasswordComponent } from './views/forgetpassword/forgetpassword.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { OffersComponent } from './views/offers/offers.component';
+import { OrdersComponent } from './views/orders/orders.component';
+import { PaymentHistoryComponent } from './views/payment-history/payment-history.component';
+import { PaymentRequestsComponent } from './views/payment-requests/payment-requests.component';
+import { PhoneotpComponent } from './views/phoneotp/phoneotp.component';
+import { ProductsCsvComponent } from './views/products-csv/products-csv.component';
+import { ProductsComponent } from './views/products/products.component';
+import { RefundComponent } from './views/refund/refund.component';
+import { ReturnOrdersComponent } from './views/return-orders/return-orders.component';
+import { SubCategoriesComponent } from './views/sub-categories/sub-categories.component';
+import { UsersComponent } from './views/users/users.component';
+import { WalletHistoryComponent } from './views/wallet-history/wallet-history.component';
 import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
-import { SubCategoriesComponent } from './components/sub-categories/sub-categories.component';
 import { TopnavComponent } from './components/topnav/topnav.component';
-import { UsersComponent } from './components/users/users.component';
-import { WalletHistoryComponent } from './components/wallet-history/wallet-history.component';
 
 
 const routes: Routes = [
-  {path: '', component: SubCategoriesComponent },
-  {path: 'login', component: LoginComponent},
-  {path: 'fg', component: ForgetpasswordComponent},
-  {path: 'enter-otp', component: PhoneotpComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'orders', component: OrdersComponent},
-  {path: 'returns', component: ReturnOrdersComponent },
-  {path: 'refunds', component: RefundComponent},
-  {path: 'customers', component: CustomersComponent},
-  {path: 'products', component: ProductsComponent},
-  {path: 'add-products', component : AddProductsComponent},
-  {path: 'upload-csv', component : ProductsCsvComponent},
-  {path: 'categories', component: CategoriesComponent},
-  {path: 'add-category',  component: AddCategoryComponent},
-  {path: 'sub-categories', component: SubCategoriesComponent},
-  {path: 'add-sub', component: AddSubComponent},
-  {path: 'add-brands', component: AddBrandsComponent},
-  {path: 'brands', component: BrandsComponent},
-  {path: 'offers', component: OffersComponent},
-  {path: 'add-offers', component: AddOffersComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'add-users', component: AddUsersComponent},
-  {path: 'payment-history', component: PaymentHistoryComponent},
-  {path: 'wallet-history', component: WalletHistoryComponent},
-  {path: 'payment-request', component: PaymentRequestsComponent},
-  {path: 'add-wallet', component: AddWalletComponent},
-  {path: 'finance-stats', component: FinanceStatsComponent}
+  { path: '', component: SubCategoriesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'fg', component: ForgetpasswordComponent },
+  { path: 'enter-otp', component: PhoneotpComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'returns', component: ReturnOrdersComponent },
+  { path: 'refunds', component: RefundComponent },
+  { path: 'customers', component: CustomersComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'add-products', component: AddProductsComponent },
+  { path: 'upload-csv', component: ProductsCsvComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'add-category', component: AddCategoryComponent },
+  { path: 'sub-categories', component: SubCategoriesComponent },
+  { path: 'add-sub', component: AddSubComponent },
+  { path: 'add-brands', component: AddBrandsComponent },
+  { path: 'brands', component: BrandsComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'add-offers', component: AddOffersComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'add-users', component: AddUsersComponent },
+  { path: 'payment-history', component: PaymentHistoryComponent },
+  { path: 'wallet-history', component: WalletHistoryComponent },
+  { path: 'payment-request', component: PaymentRequestsComponent },
+  { path: 'add-wallet', component: AddWalletComponent },
+  { path: 'finance-stats', component: FinanceStatsComponent }
 ];
 
 @NgModule({
@@ -66,4 +66,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [LoginComponent, ForgetpasswordComponent, PhoneotpComponent, HomeComponent, OrdersComponent,  ReturnOrdersComponent, RefundComponent, CustomersComponent, ProductsComponent, TopnavComponent, SidenavbarComponent, AddProductsComponent, ProductsCsvComponent, CategoriesComponent, AddCategoryComponent, SubCategoriesComponent, AddSubComponent, BrandsComponent, AddBrandsComponent, OffersComponent, AddOffersComponent, UsersComponent, AddUsersComponent, PaymentHistoryComponent, PaymentRequestsComponent, WalletHistoryComponent, AddWalletComponent, FinanceStatsComponent]
+export const routingComponents = [LoginComponent, ForgetpasswordComponent, PhoneotpComponent, HomeComponent, OrdersComponent, ReturnOrdersComponent, RefundComponent, CustomersComponent, ProductsComponent, TopnavComponent, SidenavbarComponent, AddProductsComponent, ProductsCsvComponent, CategoriesComponent, AddCategoryComponent, SubCategoriesComponent, AddSubComponent, BrandsComponent, AddBrandsComponent, OffersComponent, AddOffersComponent, UsersComponent, AddUsersComponent, PaymentHistoryComponent, PaymentRequestsComponent, WalletHistoryComponent, AddWalletComponent, FinanceStatsComponent]
--- xog-admin/src/app/app.module.ts
+++ xog-admin/src/app/app.module.ts
@@ -5,35 +5,43 @@ import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { ForgetpasswordComponent } from './components/forgetpassword/forgetpassword.component';
-import { PhoneotpComponent } from './components/phoneotp/phoneotp.component';
-import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { ForgetpasswordComponent } from './views/forgetpassword/forgetpassword.component';
+import { PhoneotpComponent } from './views/phoneotp/phoneotp.component';
+import { HomeComponent } from './views/home/home.component';
+import { OrdersComponent } from './views/orders/orders.component';
+import { ReturnOrdersComponent } from './views/return-orders/return-orders.component';
+import { RefundComponent } from './views/refund/refund.component';
+import { CustomersComponent } from './views/customers/customers.component';
+import { ProductsComponent } from './views/products/products.component';
+import { AddProductsComponent } from './views/add-products/add-products.component';
+import { ProductsCsvComponent } from './views/products-csv/products-csv.component';
+import { CategoriesComponent } from './views/categories/categories.component';
+import { AddCategoryComponent } from './views/add-category/add-category.component';
+import { SubCategoriesComponent } from './views/sub-categories/sub-categories.component';
+import { AddSubComponent } from './views/add-sub/add-sub.component';
+import { BrandsComponent } from './views/brands/brands.component';
+import { AddBrandsComponent } from './views/add-brands/add-brands.component';
+import { OffersComponent } from './views/offers/offers.component';
+import { AddOffersComponent } from './views/add-offers/add-offers.component';
+import { UsersComponent } from './views/users/users.component';
+import { AddUsersComponent } from './views/add-users/add-users.component';
+import { PaymentHistoryComponent } from './views/payment-history/payment-history.component';
+import { WalletHistoryComponent } from './views/wallet-history/wallet-history.component';
+import { PaymentRequestsComponent } from './views/payment-requests/payment-requests.component';
+import { AddWalletComponent } from './views/add-wallet/add-wallet.component';
+import { FinanceStatsComponent } from './views/finance-stats/finance-stats.component';
 import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
-import { OrdersComponent } from './components/orders/orders.component';
-import { ReturnOrdersComponent } from './components/return-orders/return-orders.component';
-import { RefundComponent } from './components/refund/refund.component';
-import { CustomersComponent } from './components/customers/customers.component';
-import { ProductsComponent } from './components/products/products.component';
-import { AddProductsComponent } from './components/add-products/add-products.component';
-import { ProductsCsvComponent } from './components/products-csv/products-csv.component';
-import { CategoriesComponent } from './components/categories/categories.component';
-import { AddCategoryComponent } from './components/add-category/add-category.component';
-import { SubCategoriesComponent } from './components/sub-categories/sub-categories.component';
-import { AddSubComponent } from './components/add-sub/add-sub.component';
-import { BrandsComponent } from './components/brands/brands.component';
-import { AddBrandsComponent } from './components/add-brands/add-brands.component';
-import { OffersComponent } from './components/offers/offers.component';
-import { AddOffersComponent } from './components/add-offers/add-offers.component';
-import { UsersComponent } from './components/users/users.component';
-import { AddUsersComponent } from './components/add-users/add-users.component';
-import { PaymentHistoryComponent } from './components/payment-history/payment-history.component';
-import { WalletHistoryComponent } from './components/wallet-history/wallet-history.component';
-import { PaymentRequestsComponent } from './components/payment-requests/payment-requests.component';
-import { AddWalletComponent } from './components/add-wallet/add-wallet.component';
-import { FinanceStatsComponent } from './components/finance-stats/finance-stats.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgSelectModule,
+    NgxDaterangepickerMd.forRoot(),
+    FormsModule
+  ],
   declarations: [
     AppComponent,
     routingComponents,
@@ -49,14 +57,22 @@ import { FinanceStatsComponent } from './components/finance-stats/finance-stats.
     WalletHistoryComponent,
     PaymentRequestsComponent,
     AddWalletComponent,
-    FinanceStatsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgSelectModule,
-    NgxDaterangepickerMd.forRoot(),
-    FormsModule,
+    FinanceStatsComponent,
+    TopnavComponent,
+    LoginComponent,
+    ForgetpasswordComponent,
+    PhoneotpComponent,
+    HomeComponent,
+    SidenavbarComponent,
+    OrdersComponent,
+    ReturnOrdersComponent,
+    RefundComponent,
+    CustomersComponent,
+    ProductsComponent,
+    AddProductsComponent,
+    ProductsCsvComponent,
+    CategoriesComponent,
+    AddCategoryComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
